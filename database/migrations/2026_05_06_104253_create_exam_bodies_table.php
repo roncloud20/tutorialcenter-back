abe05bc84c4ccfbb0d8b7bf6577b2c0b096a974f
Olugbenga Raymond <tutorialcenter001@gmail.com>
2026-05-06 19:05:18 +0100

    Cleaning up the exam body table

--- database/migrations/2026_05_06_104253_create_exam_bodies_table.php
+++ database/migrations/2026_05_06_104253_create_exam_bodies_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('status')->default('active');
             $table->softDeletes();
             $table->timestamps();
+            $table->unique(['course_id', 'slug']);
         });
     }
 
